6852a3f4b4aa36c08071aa52cc65e391e77716da
Yu Kamiya <fuzzy31u@gmail.com>
2024-02-24 11:19:27 +0900

    add: learning LLM apps with Udemy

---
+++ pages/post_x.py
@@ -0,0 +1,71 @@
+import os
+from datetime import datetime
+from typing import Optional
+
+import langchain
+import requests
+import streamlit as st
+from langchain.agents import AgentType, initialize_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.tools import tool
+from pydantic import BaseModel, Field
+from zoneinfo import ZoneInfo
+
+langchain.verbose = True
+
+
+class GoogleCalendarAddEventArgs(BaseModel):
+    event_name: str = Field(examples=["会議"])
+    start_at: str = Field(examples=["2023-11-04T19:00:00+09:00"])
+    duration: Optional[str] = Field(description="HH:mm", examples=["01:00", "02:00"])
+
+class XPostEventArgs(BaseModel):
+    post_content: str = Field(examples=["「Cloud RunサイドカーとURLマスクを活用したWINTICKET Webの検証環境改善」記事では、検証環境の容易なセットアップについて解説しています。Cloud RunサイドカーとURLマスクの利用法に焦点を当て、改善策について具体的に紹介しています。"])
+
+@tool("google-calendar-add-event", args_schema=GoogleCalendarAddEventArgs)
+def google_calendar_add_event_tool(
+    event_name: str, start_at: str, duration: Optional[str]
+):
+    """Google Calendar Add Event"""
+    webhook_url = os.environ["MAKE_WEBHOOK_CALENDAR_URL"]
+    body = {
+        "eventName": event_name,
+        "startAt": start_at,
+        "duration": duration,
+    }
+    result = requests.post(webhook_url, json=body)
+    return f"Status: {result.status_code} - {result.text}"
+
+
+@tool("clock")
+def clock_tool():
+    """Clock to get current datetime"""
+    return datetime.now(ZoneInfo("Asia/Tokyo")).isoformat()
+
+@tool("x-post-event", args_schema=XPostEventArgs)
+def x_post_event_tool(
+    post_content: str
+):
+    """X Post Event"""
+    webhook_url = os.environ["MAKE_WEBHOOK_X_URL"]
+    body = {
+        "postContent": post_content,
+    }
+    result = requests.post(webhook_url, json=body)
+    return f"Status: {result.status_code} - {result.text}"
+
+
+st.title("X投稿アシスタント")
+
+input = st.text_input(label="何を依頼しますか？")
+
+if input:
+    with st.spinner("投稿中..."):
+        llm = ChatOpenAI(model="ft:gpt-3.5-turbo-0613:personal::8uIGbfNa", temperature=0)
+        agent = initialize_agent(
+            tools=[google_calendar_add_event_tool, clock_tool, x_post_event_tool],            
+            llm=llm,
+            agent=AgentType.OPENAI_FUNCTIONS,
+        )
+        result = agent.run(input)
+        st.write(result)
